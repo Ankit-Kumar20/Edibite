51ede8fac5b9cd006045129f818fc4df5d39cea2
SwastikGiri <124946186+SwastikGiri@users.noreply.github.com>
2024-10-18 09:38:16 +0530

    Update sidebar.jsx

--- frontend/src/restaurant/components/Sidebar/sidebar.jsx
+++ frontend/src/restaurant/components/Sidebar/sidebar.jsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { IoMdAdd } from "react-icons/io";
+import { MdChecklist } from "react-icons/md";
+import { HiShoppingCart } from "react-icons/hi";
+
+
 
 const Sidebar = () => {
   return (
-    <div className="w-1/5 min-h-screen border border-gray-400 border-t-0">
-      <div className="pt-12 pl-10 flex flex-col gap-5">
-        <NavLink to="/add" className="flex items-center gap-3 border border-gray-400 border-r-0 py-2 px-3 rounded-l cursor-pointer hover:bg-gray-200 active:bg-white">
-          <img src="/path-to-your-assets/add_icon.png" alt="Add" className="w-5" />
-          <p className="hidden lg:block">Add Items</p>
-        </NavLink>
-        <NavLink to="/list" className="flex items-center gap-3 border border-gray-400 border-r-0 py-2 px-3 rounded-l cursor-pointer hover:bg-gray-200 active:bg-white">
-          <img src="/path-to-your-assets/order_icon.png" alt="List" className="w-5" />
-          <p className="hidden lg:block">List Items</p>
-        </NavLink>
-        <NavLink to="/orders" className="flex items-center gap-3 border border-gray-400 border-r-0 py-2 px-3 rounded-l cursor-pointer hover:bg-gray-200 active:bg-white">
-          <img src="/path-to-your-assets/order_icon.png" alt="Orders" className="w-5" />
-          <p className="hidden lg:block">Orders</p>
-        </NavLink>
-      </div>
-    </div>
+    <ul className="menu bg-black md:text-xl text-white rounded-box w-full">
+  <li>
+    <details >
+      <summary><IoMdAdd />Add Items</summary>
+      <ul>
+        <li><a>Item 1</a></li>
+        <li><a>Item 2</a></li>
+      </ul>
+    </details>
+  </li>
+  <li>
+    <details >
+      <summary><MdChecklist />List Items</summary>
+      <ul>
+        <li><a>Item 1</a></li>
+        <li><a>Item 2</a></li>
+        
+      </ul>
+    </details>
+  </li>
+  <li>
+    <details >
+      <summary><HiShoppingCart />
+      Orders</summary>
+      <ul>
+        <li><a>Order 1</a></li>
+        <li><a>Order 2</a></li>
+        <li>
+          <details >
+            <summary>Order 3</summary>
+            <ul>
+              <li><a>SubOrder 1</a></li>
+              <li><a>SubOrder 2</a></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </details>
+  </li>
+</ul>
   );
 };
 
