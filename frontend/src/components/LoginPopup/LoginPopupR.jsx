13319d0782a34856034a90d2dc312172dad11394
Ayush Ranjan Sahoo <77970726+aysahoo@users.noreply.github.com>
2024-12-09 16:46:01 +0530

    Updated Login of Restaurants
    Input variables work done!!!

--- frontend/src/components/LoginPopup/LoginPopupR.jsx
+++ frontend/src/components/LoginPopup/LoginPopupR.jsx
@@ -1,7 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ModalR = ({ isOpen, onClose }) => {
   const [isLogin, setIsLogin] = useState(true);
+
+  const [rData, setrData] = useState({
+    name: "",
+    mobile: "",
+    email: "",
+    password: "",
+  });
+  const [lData, setlData] = useState({
+    email:"",
+    password: "",
+  });
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setrData({ ...rData, [name]: value });
+  }
+  const onChangeHandlerL = (event) => {
+    const nameL = event.target.name;
+    const value = event.target.value;
+    setlData({ ...lData, [nameL]: value });
+  }
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    console.log(lData);
+  }
+  const onRegister = (e) => {
+    e.preventDefault();
+    console.log(rData);
+  }
 
   if (!isOpen) return null;
 
@@ -15,18 +45,25 @@ const ModalR = ({ isOpen, onClose }) => {
         {isLogin ? (
           <div>
             <h2 className="text-2xl font-semibold mb-4 text-white">Welcome Again</h2>
-            <form className="space-y-4">
+            <form onSubmit={onLogin} className="space-y-4">
               <div>
-                <label className="block text-sm text-white font-medium  ">Restaurant ID</label>
+                <label className="block text-sm text-white font-medium  ">Restaurant Email ID</label>
                 <input
+                  name='email'
+                  onChange={onChangeHandlerL}
+                  value={lData.email}
+                  type="email"
                   className="w-full p-2 rounded-md focus:outline-none focus:ring-[#B9FF66] focus:ring-1 placeholder:opacity-30"
-                  placeholder="Enter your unique ID"
+                  placeholder="Enter your Email ID"
                   required
                 />
               </div>
               <div>
                 <label className="block text-sm text-white  font-medium">Password</label>
                 <input
+                name='password'
+                onChange={onChangeHandlerL}
+                value={lData.password}
                   type="password"
                   className="w-full p-2 rounded-md focus:outline-none focus:ring-[#B9FF66] focus:ring-1 placeholder:opacity-30"
                   placeholder="Enter your password"
@@ -53,10 +90,13 @@ const ModalR = ({ isOpen, onClose }) => {
         ) : (
           <div>
             <h2 className="text-2xl font-semibold mb-4 text-white">Welcome to Edibite</h2>
-            <form className="space-y-4">
+            <form onSubmit={onRegister}  className="space-y-4">
               <div>
                 <label className="block text-sm font-medium text-white">Restaurant Name</label>
                 <input
+                  name='name'
+                  onChange={onChangeHandler}
+                  value={rData.name}
                   type="text"
                   className="w-full p-2 rounded-md focus:outline-none focus:ring-[#B9FF66] focus:ring-1 placeholder:opacity-30"
                   placeholder="Enter Restaurant name"
@@ -66,7 +106,10 @@ const ModalR = ({ isOpen, onClose }) => {
               <div>
                 <label className="block text-sm font-medium text-white">Mobile No.</label>
                 <input
-                  type="email"
+                  name='mobile'
+                  onChange={onChangeHandler}
+                  value={rData.mobile}
+                  type="number"
                   className="w-full p-2 rounded-md focus:outline-none focus:ring-[#B9FF66] focus:ring-1 placeholder:opacity-30"
                   placeholder="Enter your mobile number"
                   required
@@ -75,6 +118,9 @@ const ModalR = ({ isOpen, onClose }) => {
               <div>
                 <label className="block text-sm font-medium text-white">Email</label>
                 <input
+                  name='email'
+                  onChange={onChangeHandler}
+                  value={rData.email}
                   type="email"
                   className="w-full p-2 rounded-md focus:outline-none focus:ring-[#B9FF66] focus:ring-1 placeholder:opacity-30"
                   placeholder="Enter your email"
@@ -84,6 +130,9 @@ const ModalR = ({ isOpen, onClose }) => {
               <div>
                 <label className="block text-sm font-medium text-white">Password</label>
                 <input
+                  name='password'
+                  onChange={onChangeHandler}
+                  value={rData.password}
                   type="password"
                   className="w-full p-2 rounded-md focus:outline-none focus:ring-[#B9FF66] focus:ring-1 placeholder:opacity-30"
                   placeholder="Create a password"
